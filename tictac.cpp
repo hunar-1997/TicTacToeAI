bab96d3dfccae7a60792ef5e5ec118b3bf58f414
hunar1997 <hbkurd@gmail.com>
2016-11-11 11:35:20 +0300

    AI function almost finished .. only recursion left (i think) .. the game managment too

--- tictac.cpp
+++ tictac.cpp
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-short Board[9]={1, 0, 1,
-				0, 0, 0,
-				1, 0, 0};
+int Board[9]={0, 0, 1,
+			  0, 1, 0,
+			  0, 0, 0};
 const char s[] = "X 0";
 
-void printBoard(short b[]){
+void printBoard(int b[]){ // print the board
 	printf("-----\n");
-	for(short j=0;j<3;++j, printf("\n"))
-		for(short i=0;i<3;++i)
+	for(int j=0;j<3;++j, printf("\n"))
+		for(int i=0;i<3;++i)
 			printf("%c", s[ b[i+j*3]+1 ] );
 }
 
-short checkWin(short b[]){
-	for(short i=0;i<3;++i){
-		short z = b[i*3] + b[i*3+1] + b[i*3+2];
+int checkWin(int b[]){ //return the player that won
+	for(int i=0;i<3;++i){
+		int z = b[i*3] + b[i*3+1] + b[i*3+2];
 		if(z==3 || z==-3) return z/3;
 		
 		z = b[i] + b[3+i] + b[6+i];
@@ -24,35 +24,74 @@ short checkWin(short b[]){
 	if( b[0]==b[4] && b[4]==b[8] ) return b[4];
 	if( b[2]==b[4] && b[4]==b[6] ) return b[4];
 	
-	return 0;
+	return 0; // zero means no one
 }
 
-short playerMove(){
-	short r;
+int playerMove(){ // ask user for input
+	int r;
 	printf("Your turn: ");
 	while(true){
 		if( scanf("%d", &r) ) 
 			if(r>0 && r<10)
-				break;
+				if(Board[r]==0)
+					break;
+				else printf("Not possible");
 			else printf("Must be between 1&9, ");
 		else printf("Not a number, ");
 		printf("try again: ");
 	}
-	return r;
+	return r; // return the selected move's position
 }
 
-short aiMove(short difficulty){
-	return 0;
+struct Data{
+	int score=0;
+	int position;
+};
+
+int aiMove(int b[], int turn){ // AI movement
+	int available[9]; // empty positions
+	int count=0;
+	
+	for(int i=0;i<9;++i)
+		if( b[i]==0 )
+			available[count++]=i;
+	
+	if(count==0) return 10; // no move available
+	
+	Data result[9];
+	for(int i=0;i<count;++i){
+		// calculate this position
+		b[ available[i] ] = turn;
+		int ws = checkWin(b);
+	
+		if(ws==turn){
+			result[ available[i] ].score += 100;
+			result[ available[i] ].position = available[i];
+		}
+		// reset table
+		b[ available[i] ] = 0;
+	}
+	
+	int highest = -100, pos=10; // select the one with highest score
+	for(int i=0;i<count;++i)
+		if(result[available[i]].score>highest){
+			highest = result[available[i]].score;
+			pos = result[available[i]].position;
+		}
+	
+	return pos;
 }
 
 
 int main(){
 	bool stillPlaying=true;
+	
+	printf( "win location %d", aiMove(Board, 1)+1 );
+	
 	while( stillPlaying ){
-		short move = playerMove();
 		break;
+		int move = playerMove();
 	}
-	printf("no body wins\n");
 	
 	return 0;
 }
